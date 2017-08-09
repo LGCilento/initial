d4605362f1415476804765ef9055153c38be3daf
lgc3 <lgc3@cin.ufpe.br>
2017-08-09 09:28:32 -0300

    ex1

---
+++ new.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(int argc, char const *argv[])
+{
+	printf("oi\n", );
+}
+
+void print_(){
+	printf("oi\n");
+}
+void count(int limit){
+	for (int i = 0; i < limit; ++i)
+	{
+		printf("%d\n",i );
+	}
+}
